848ce9cc7b0f7c2b926a4662c4b67687a80a2976
OleKL <oleklar@stud.ntnu.no>
2018-10-12 08:45:41 +0200

    New project

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# findyourigcinfohere
+# README
+
+The Project named GetIgcInfoGO will allow the user to access a deployed application on Heroku, 
+and then gather required information regarding a logged flight from an IGC formatted file.
+
+
+**Main file: igc.go**
+
+The file is commented all the way through in a minimalistic manner, to easiest explain the meaning 
+behind the structure without disrupting the flow or making a clutter of the code.
+
+
+**Libraries used:**
+
+1. github.com/marni/goigc	
+2. github.com/p3lim/iso8601	
+
+
+
+The following sections of code are used as alternatives.
+
+First section: Alternative for formatting 'uptime' into ISO 8601, 
+if the included library would no longer be dependent.
+
+
+```
+S := int(time.Since(startTime) / time.Second)	// Gather number of seconds since start
+
+Mi := S/60		// Calculate Minutes, Hours, Days, Months and Years based on seconds.
+H :=  Mi/60
+D :=  H/24
+Mo := D/30
+Y :=  Mo/12
+              	                // Remove values when times reach max.			
+for year := 0; year < Y; year ++ { 	       Mo -= 12  }
+for month := 0; month < Mo; month ++ { 	   D  -= 30  }
+for day := 0; day < D; day ++ { 	       H  -= 24  }
+for hour := 0; hour < H; hour ++ { 	       Mi -= 60  }
+for minute := 0; minute < Mi; minute ++ {  S  -= 60  } 
+
+              	                // Create string with ISO 8601 format
+time := "P"+strconv.Itoa(Y)+"Y"+strconv.Itoa(Mo)+"M"+strconv.Itoa(D)+"DT"+strconv.Itoa(H)+"H"+strconv.Itoa(Mi)+"M"+strconv.Itoa(S)+"S"
+
+temp := MetaData{time, "Service for IGC tracks.", "v1"}
+```
+
+Second section: Alternative version for ouput TrackIDs. This code encodes each struct instead 
+of printing the stored IDs as an int array.
+
+
+```
+for id := range TrackIds {
+  temp := TrackId{id}
+          // Encodes temporary struct and shows information on screen
+  err := json.NewEncoder(w).Encode(temp)
+  if(err != nil){
+      fmt.Println("\nEncode Error:", err)
+  }
+}
+```
+
+
+Third section: Alternative way to print IDs, direcly from stored URLs, by storing
+placement numbers in an integer slice.
+
+
+```
+var a []int
+for key, url := range TrackUrl {
+  a = append(a, key)
+  fmt.Println("\n", url)
+}
+fmt.Fprintln(w, a)
+```
---
+++ igc.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"strconv"
+	"time"
+	"log"
+	"os"
+	"github.com/marni/goigc"		// Main library for working on IGC files
+	"github.com/p3lim/iso8601"	// For formatting time into ISO 8601
+)
+
+
+var TrackUrl map[int]string		// Declare map for storing URLs
+var TrackIds map[int]int			// Declare map for storing IDs corresponding to URL
+var startTime time.Time				// Variable for calculating uptime
+
+
+type TrackInfo struct {				// Encoding complete IGC profile
+	H_date string					`json:"date"`
+	Pilot string					`json:"pilot"`
+	Glider string					`json:"glider"`
+	Glider_id string 			`json:"glider_id"`
+	Track_length float64	`json:"track_length"`
+}
+
+
+type MetaData struct {		// Encoding meta information of server
+	Uptime 	string	`json:"uptime"`
+	Info 		string	`json:"info"`
+	Version string	`json:"version"`
+}
+
+
+type TrackId struct {			// Encoding URL ID
+	Id int	`json: "id"`
+}
+
+
+func handleIgcPlus(w http.ResponseWriter, r *http.Request) {
+
+	if (r.Method == http.MethodGet) {	// Check if GET was called
+
+		parts := strings.Split(r.URL.Path, "/")	// Storing URL parts in a new array
+		l := len(parts)				// Number of parts
+		t := len(TrackIds)		// Number of already stored IDs
+		idString := parts[4]	// Stores ID from 5th element in a string
+
+		id, err := strconv.Atoi(idString)	// Converts to int
+		if (err != nil) {
+			fmt.Printf("\nConverting Error", err)
+		}
+
+		url := TrackUrl[id]	// Gets the correct URL based on input id
+
+		track, err2 := igc.ParseLocation(url)	// Parses information from URL location
+		if(err2 != nil){
+				fmt.Println("\nParse Error:", err2)
+		}																			// Calculates track length
+		trackLen := track.Points[0].Distance(track.Points[len(track.Points)-1])
+
+		// WITH FIELD
+		if (l == 6 && id > 0 && id <= t) {					// If the call consists of 6 parts
+			fmt.Printf("\nGET /api/igc/<Id>/<Field>") // Write to console
+
+			field := parts[5]				// Store the field input
+
+			if (field == "pilot") {	// Check which variable 'field' is equal to
+				fmt.Fprintln(w, track.Pilot)
+
+			} else if (field == "glider") {
+				fmt.Fprintln(w, track.GliderType)
+
+			} else if (field == "glider_id") {
+				fmt.Fprintln(w, track.GliderID)
+
+			} else if (field == "track_length") {
+				fmt.Fprintln(w, trackLen)
+
+			} else if (field == "H_date") {
+				fmt.Fprintln(w, track.Date.String())
+
+			} else {	// If neither of the correct variables are called: 404
+				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			}
+
+		// ONLY ID, EMPTY FIELD
+		} else if (l == 5 && id > 0 && id <= t) {	// 5 parts and id input is valid (1-len)
+			fmt.Printf("\nGET /api/igc/<Id>") 			// Write to console
+								// Creating temporary struct to hold variables
+			temp := TrackInfo{track.Date.String(), track.Pilot, track.GliderType, track.GliderID, trackLen}
+								// Encodes temporary struct and shows information on screen
+			http.Header.Add(w.Header(), "Content-type", "application/json")
+			err3 := json.NewEncoder(w).Encode(temp)
+			if(err3 != nil){
+			  	fmt.Println("\nEncode Error:", err3)
+			}
+		// If neither ID or ID + Field was found: 404
+		} else {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		}
+	}
+}
+
+
+func handleIgc(w http.ResponseWriter, r *http.Request) {
+
+		if (r.Method == http.MethodGet) {	// Check if GET was called
+			fmt.Printf("\nGET /api/igc") 		// Write to console
+
+			var a []int
+			for key, url := range TrackUrl {// Append each key in TrackUrl to the slice 'a'
+				a = append(a, key)
+				fmt.Println("\n", url)				// To avoid console error of URL not used.
+			}
+			w.Header().Set("Content-Type", "application/json")
+			err := json.NewEncoder(w).Encode(a)
+			if(err != nil){
+			  	fmt.Println("\nEncode Error:", err)
+			}
+
+		} else if (r.Method == http.MethodPost) {	// Check if POST was called
+			fmt.Printf("\nPOST /api/igc")						// Write to console
+
+			var temp map[string]interface{}	// Interface is unknown type / C++ auto
+
+			err2 := json.NewDecoder(r.Body).Decode(&temp)	// Decode posted url
+			if (err2 != nil) {
+				fmt.Printf("\nDecode Error", err2)
+			}
+														// Internal identification: Int up from 1
+			tempLen := len(TrackUrl) + 1
+														// Places url in map spot nr 1 and up
+			TrackUrl[tempLen] = temp["url"].(string)
+			TrackIds[tempLen] = tempLen
+
+			idStruct := TrackId{Id: tempLen}
+														// Define header for correct output
+			http.Header.Add(w.Header(), "Content-type", "application/json")
+			err3 := json.NewEncoder(w).Encode(idStruct)
+			if (err3 != nil) {
+				fmt.Printf("\nEncode Error", err3)
+			}
+		}
+}
+
+
+func handleApi(w http.ResponseWriter, r *http.Request) {
+	if (r.Method == http.MethodGet) {	// Check if GET was called
+
+		fmt.Printf("\nGET /api")	// Write to console
+
+									// Using included library, format time Since into ISO 8601
+		t := iso8601.Format(time.Since(startTime))
+
+									// Prepare struct for encoding
+		temp := MetaData{t, "Service for IGC tracks.", "v1"}
+
+									// Define header for correct output
+		http.Header.Add(w.Header(), "Content-type", "application/json")
+		err := json.NewEncoder(w).Encode(temp)
+		if(err != nil){
+				fmt.Println("\nEncode Error:", err)
+		}
+	}
+}
+
+		// Handle every call beginning with anything else than igcinfo/api
+func handleInvalid(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+}
+
+				// Find the correct Port to run app on Heroku
+func GetPort() string {
+	 	var port = os.Getenv("PORT")
+ 				// Port sets to :8080 as a default
+ 		if (port == "") {
+ 			port = "8080"
+			fmt.Println("No PORT variable detected, defaulting to " + port)
+ 		}
+ 		return (":" + port)
+}
+
+
+func main() {
+
+		TrackUrl = make(map[int]string) // Initializing map arrays
+		TrackIds = make(map[int]int)
+		startTime = time.Now()	// Initializes timer
+
+		http.HandleFunc("/", handleInvalid)
+		http.HandleFunc("/igcinfo/api", handleApi)
+		http.HandleFunc("/igcinfo/api/igc", handleIgc)
+		http.HandleFunc("/igcinfo/api/igc/", handleIgcPlus)
+
+		fmt.Printf("\nPing")		// Server opened
+
+		err := http.ListenAndServe(GetPort(), nil)
+		// err := http.ListenAndServe("localhost:8080", nil);
+		if err != nil {
+				log.Fatal("ListenAndServe Error: ", err)
+		}
+}
